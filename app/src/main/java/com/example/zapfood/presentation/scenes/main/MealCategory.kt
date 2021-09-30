32d4402caa5d5d98ea22fb8831c856564b59ef2d
jvsena42 <jvsena16@gmail.com>
2021-09-30 07:47:41 -0300

    fix: animate expand lines

--- app/src/main/java/com/example/zapfood/presentation/scenes/main/MealCategory.kt
+++ app/src/main/java/com/example/zapfood/presentation/scenes/main/MealCategory.kt
@@ -1,14 +1,25 @@
 package com.example.zapfood.presentation.scenes.main
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -17,6 +28,7 @@ import com.example.zapfood.data.model.response.Category
 @ExperimentalCoilApi
 @Composable
 fun MealCategory(category: Category) {
+    var isExpanded by remember { mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         elevation = 2.dp,
@@ -24,20 +36,47 @@ fun MealCategory(category: Category) {
             .fillMaxWidth()
             .padding(top = 16.dp)
     ) {
-        Row {
+        Row(modifier = Modifier.animateContentSize()) {
             Image(
                 painter = rememberImagePainter(category.categoryThumb),
                 contentDescription = null,
                 modifier = Modifier
                     .size(88.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
-            Column(modifier = Modifier
-                .align(Alignment.CenterVertically)
-                .padding(16.dp)) {
-                Text(text = category.category ?: "",
-                style = MaterialTheme.typography.h6)
+            Column(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
+                    .padding(16.dp)
+            ) {
+
+                Text(
+                    text = category.category ?: "",
+                    style = MaterialTheme.typography.h6
+                )
+                Text(
+                    text = category.categoryDescription ?: "",
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.subtitle2,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.LightGray,
+                    maxLines = if (isExpanded) 10 else 3
+                )
+
             }
+            Icon(
+                imageVector = if (isExpanded)
+                    Icons.Filled.KeyboardArrowUp
+                 else
+                    Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand row icon",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .align(Alignment.CenterVertically)
+                    .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
